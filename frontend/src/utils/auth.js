19c14fc57ea3e9476b496af8c1cf6928acd35d7f
ddubinin <dubinin.dmitry@mail.ru>
2022-07-08 14:41:17 +0300

    fix: base_url

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://api.ddubinin.students.nomoredomains.xyz";
+export const BASE_URL = "https://api.ddubinin.students.nomoredomains.xyz";
 
 const checkResponce = (res) => {
   if(res.ok) {
