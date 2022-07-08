e059e964c0d5e498a5d9144ef7d2e37d3e413ac4
ddubinin <dubinin.dmitry@mail.ru>
2022-07-08 14:20:40 +0300

    feat: change base_url

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://auth.nomoreparties.co";
+export const BASE_URL = "http://api.ddubinin.students.nomoredomains.xyz/";
 
 const checkResponce = (res) => {
   if(res.ok) {
