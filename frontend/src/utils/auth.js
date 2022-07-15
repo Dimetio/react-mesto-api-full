4a12b0256725c3175642c8f6d00f5d3df30d9099
ddubinin <dubinin.dmitry@mail.ru>
2022-07-15 13:43:18 +0300

    fix: change default url localhost

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = `${window.location.protocol}${process.env.REACT_APP_API_URL || '//localhost:3001'}`;
+export const BASE_URL = `${window.location.protocol}${process.env.REACT_APP_API_URL || '//localhost:3000'}`;
 
 const checkResponce = (res) => {
   if(res.ok) {
